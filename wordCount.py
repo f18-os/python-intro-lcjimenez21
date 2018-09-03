b43fea8ce5b6cc5e18d311e5bdbb634dd5f8ca1b
lcjimenez21 <lcjimenez2119@gmail.com>
2018-09-03 15:06:40 -0600

    fix bug causing problems with createDict method

--- wordCount.py
+++ wordCount.py
@@ -10,34 +10,42 @@ def getList():
     if os.path.exists(input):
         inputFile = open((input),"r")
         data = inputFile.read().replace('\n',' ')
+        if '-' in data: #break words with '-'
+            data.replace('-', ' ')
         data = re.sub('\W+',' ', data)
+        data = re.sub('\s+', ' ', data)
         newData = data.split(' ')
         inputFile.close()
-        print("pass")
+        print("Pass") #debug tool
         createDict(newData)
     else:
         print("The file %s doesn't exist" % input)
 
-    # inputFile = open((input),"r")
-    # data = readline.replace('\n',' ')
-    # data = re.sub('\W+', ' ')
-    # inputFile.close()
-
 
 def createDict(newData):
     myDict = {}
+    #add words to dictionary
     for word in newData:
-        if word in myDict:
+        #if word exist repetition has occur keep count of repetions
+        if word.lower() in myDict:
             myDict[word.lower()] += 1
-        else:
-            myDict[word.lower()] = 1
+            pass
 
+        #if a nothing(blank space) occurs just ignore, add instance of word
+        elif not word == '':
+            myDict[word.lower()] = 1
+            pass
+
+    #make dictionary in alphabetical order
     dictionary_inOrder = collections.OrderedDict(sorted(myDict.items()))
 
+    '''create my output file with all words along the number of times it appear
+    on the input file'''
     outputFile = open((output), "w+")
     for word in dictionary_inOrder:
         outputFile.write('%s %d\n' % (word, dictionary_inOrder[word]))
+
     outputFile.close()
 
-################ Main method
+################ Main method ######################
 getList()
